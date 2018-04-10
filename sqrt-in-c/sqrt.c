2c199a76c4959a121b61f956651f6521d63d11db
Peter Donald <peter@realityforge.org>
2018-04-10 20:51:24 +1000

    Add some notes

--- sqrt-in-c/sqrt.c
+++ sqrt-in-c/sqrt.c
@@ -1,5 +1,7 @@
 #include <math.h>
 
+// This definition declares an expected input from import.
+// Note: if it is not used then no import will be required.
 void consoleLog (float num);
 
 float getSqrt (float num) {
